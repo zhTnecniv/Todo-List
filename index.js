4909b301c4c522ee6ee67c9e27de564272fcb0d9
zhTnecniv <not.lucretius.nature@gmail.com>
2023-03-10 16:34:24 -0500

    finished

--- index.js
+++ index.js
@@ -1,56 +1,48 @@
-//console.log("hello world")
+function myFetch(url = "", options = {}) {
+    return new Promise((res, rej) => {
+        const xhr = new XMLHttpRequest();
+        const method = options.method || "GET";
+        xhr.open(method, url);
+        xhr.setRequestHeader("Content-type", "application/json");
+        xhr.responseType = "json";
+        xhr.onload = function () {
+            res(xhr.response);
+        }
+        xhr.onerror = function () {
+            rej("error");
+        }
+        xhr.send(options.body);
+    });
+};
 
-/* 
-  client side
-    template: static template
-    logic(js): MVC(model, view, controller): used to server side technology, single page application
-        model: prepare/manage data,
-        view: manage view(DOM),
-        controller: business logic, event bindind/handling
-
-  server side
-    json-server
-    CRUD: create(post), read(get), update(put, patch), delete(delete)
-
-
-*/
-
-//read
-/* fetch("http://localhost:3000/todos")
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
-    }); */
 
 const APIs = (() => {
     const createTodo = (newTodo) => {
-        return fetch("http://localhost:3000/todos", {
+        return myFetch("http://localhost:3000/todos", {
             method: "POST",
-            body: JSON.stringify(newTodo),
-            headers: { "Content-Type": "application/json" },
-        }).then((res) => res.json());
+            body: JSON.stringify(newTodo)
+        }).then(res => res);
     };
 
     const deleteTodo = (id) => {
-        return fetch("http://localhost:3000/todos/" + id, {
+        return myFetch("http://localhost:3000/todos/" + id, {
             method: "DELETE",
-        }).then((res) => res.json());
+        }).then(res => res);
+    };
+
+    const updateTodo = (id, newTodo) => {
+        return myFetch("http://localhost:3000/todos/" + id, {
+            method: "PATCH",
+            body: JSON.stringify(newTodo)
+        }).then(res => res);
     };
 
     const getTodos = () => {
-        return fetch("http://localhost:3000/todos").then((res) => res.json());
+        return myFetch("http://localhost:3000/todos").then(res => res);
     };
-    return { createTodo, deleteTodo, getTodos };
+    return { createTodo, deleteTodo, updateTodo, getTodos };
 })();
 
-//IIFE
-//todos
-/* 
-    hashMap: faster to search
-    array: easier to iterate, has order
-
-
-*/
 const Model = (() => {
     class State {
         #todos; //private field
@@ -63,7 +55,6 @@ const Model = (() => {
         }
         set todos(newTodos) {
             // reassign value
-            console.log("setter function");
             this.#todos = newTodos;
             this.#onChange?.(); // rendering
         }
@@ -73,49 +64,63 @@ const Model = (() => {
             this.#onChange = callback;
         }
     }
-    const { getTodos, createTodo, deleteTodo } = APIs;
+    const { getTodos, createTodo, updateTodo, deleteTodo } = APIs;
     return {
         State,
         getTodos,
         createTodo,
+        updateTodo,
         deleteTodo,
     };
 })();
-/* 
-    todos = [
-        {
-            id:1,
-            content:"eat lunch"
-        },
-        {
-            id:2,
-            content:"eat breakfast"
-        }
-    ]
 
-*/
 const View = (() => {
-    const todolistEl = document.querySelector(".todo-list");
+    const pendinglistEl = document.querySelector(".pending-list");
+    const completedlistEl = document.querySelector(".completed-list");
     const submitBtnEl = document.querySelector(".submit-btn");
     const inputEl = document.querySelector(".input");
 
     const renderTodos = (todos) => {
-        let todosTemplate = "";
+        let pendingsTemplate = "";
+        let completedsTemplate = "";
         todos.forEach((todo) => {
-            const liTemplate = `<li><span>${todo.content}</span><button class="delete-btn" id="${todo.id}">delete</button></li>`;
-            todosTemplate += liTemplate;
+            const startEditBtn = `<button class="start-edit-btn" id="${todo.id}">edit</button>`;
+            const saveEditBtn = `<button class="save-edit-btn" id="${todo.id}">save</button>`;
+            const deleteBtn = `<button class="delete-btn" id="${todo.id}">delete</button>`;
+            let statusBtn = "";
+            if (todo.status === "pending") {
+                statusBtn = `<button class="status-btn" id="${todo.id}">-></button>`;
+            } else {
+                statusBtn = `<button class="status-btn" id="${todo.id}"><-</button>`;
+            }
+            let liTemplate;
+            if (!todo.editing) {
+                liTemplate = `<span>${todo.content}</span>${startEditBtn}${deleteBtn}`
+            } else {
+                liTemplate = `<input type="text" value="${todo.content}"></input>${saveEditBtn}${deleteBtn}`
+            }
+            if (todo.status === "pending") {
+                pendingsTemplate += `<li>${liTemplate}${statusBtn}</li>`;
+            } else if (todo.status === "completed") {
+                completedsTemplate += `<li>${statusBtn}${liTemplate}</li>`;
+            }
         });
-        if (todos.length === 0) {
-            todosTemplate = "<h4>no task to display!</h4>";
+        if (pendingsTemplate.length === 0) {
+            pendingsTemplate = "<h4>no task to display!</h4>";
         }
-        todolistEl.innerHTML = todosTemplate;
+        if (completedsTemplate.length == 0) {
+            completedsTemplate = "<h4>no task to display!</h4>";
+        }
+
+        pendinglistEl.innerHTML = pendingsTemplate;
+        completedlistEl.innerHTML = completedsTemplate;
     };
 
     const clearInput = () => {
         inputEl.value = "";
     };
 
-    return { renderTodos, submitBtnEl, inputEl, clearInput, todolistEl };
+    return { renderTodos, submitBtnEl, inputEl, clearInput, pendinglistEl, completedlistEl };
 })();
 
 const Controller = ((view, model) => {
@@ -129,32 +134,110 @@ const Controller = ((view, model) => {
 
     const handleSubmit = () => {
         view.submitBtnEl.addEventListener("click", (event) => {
-            /* 
-                1. read the value from input
-                2. post request
-                3. update view
-            */
             const inputValue = view.inputEl.value;
-            model.createTodo({ content: inputValue }).then((data) => {
+            model.createTodo({ content: inputValue, status: "pending" }).then((data) => {
                 state.todos = [data, ...state.todos];
                 view.clearInput();
             });
         });
     };
 
-    const handleDelete = () => {
-        //event bubbling
-        /* 
-            1. get id
-            2. make delete request
-            3. update view, remove
-        */
-        view.todolistEl.addEventListener("click", (event) => {
+    const handlePendingListDelete = () => {
+        view.pendinglistEl.addEventListener("click", (event) => {
             if (event.target.className === "delete-btn") {
                 const id = event.target.id;
-                console.log("id", typeof id);
                 model.deleteTodo(+id).then((data) => {
                     state.todos = state.todos.filter((todo) => todo.id !== +id);
+                });
+            }
+        });
+    };
+
+    const handleCompletedListDelete = () => {
+        view.completedlistEl.addEventListener("click", (event) => {
+            if (event.target.className === "delete-btn") {
+                const id = event.target.id;
+                model.deleteTodo(+id).then((data) => {
+                    state.todos = state.todos.filter((todo) => todo.id !== +id);
+                });
+            }
+        });
+    };
+
+    const handlePendingListStatus = () => {
+        view.pendinglistEl.addEventListener("click", (event) => {
+            if (event.target.className === "status-btn") {
+                const id = event.target.id;
+                model.updateTodo(+id, { status: "completed" }).then((data) => {
+                    state.todos = state.todos.map((todo) => todo.id == data.id ? data : todo);
+                });
+            }
+        });
+    };
+
+    const handleCompletedListStatus = () => {
+        view.completedlistEl.addEventListener("click", (event) => {
+            if (event.target.className === "status-btn") {
+                const id = event.target.id;
+                model.updateTodo(+id, { status: "pending" }).then((data) => {
+                    state.todos = state.todos.map((todo) => todo.id == data.id ? data : todo);
+                });
+            }
+        });
+    };
+
+    const handlePendingListStartEdit = () => {
+        view.pendinglistEl.addEventListener("click", (event) => {
+            if (event.target.className === "start-edit-btn") {
+                const id = event.target.id;
+                state.todos = state.todos.map((todo) => {
+                    if (+todo.id == +id) {
+                        const newTodo = { ...todo };
+                        newTodo.editing = true;
+                        return newTodo;
+                    }
+                    return todo;
+                });
+            }
+        });
+    };
+
+    const handleCompletedListStartEdit = () => {
+        view.completedlistEl.addEventListener("click", (event) => {
+            if (event.target.className === "start-edit-btn") {
+                const id = event.target.id;
+                state.todos = state.todos.map((todo) => {
+                    if (+todo.id == +id) {
+                        const newTodo = { ...todo };
+                        newTodo.editing = true;
+                        return newTodo;
+                    }
+                    return todo;
+                });
+            }
+        });
+    };
+
+    const handlePendingListFinishEdit = () => {
+        view.pendinglistEl.addEventListener("click", (event) => {
+            if (event.target.className === "save-edit-btn") {
+                const id = event.target.id;
+                const inputVal = event.target.parentElement.getElementsByTagName("input")[0].value
+                model.updateTodo(+id, { content: inputVal }).then((data) => {
+                    state.todos = state.todos.map((todo) => todo.id == data.id ? data : todo);
+                });
+            }
+        });
+    };
+
+
+    const handleCompletedListFinishEdit = () => {
+        view.completedlistEl.addEventListener("click", (event) => {
+            if (event.target.className === "save-edit-btn") {
+                const id = event.target.id;
+                const inputVal = event.target.parentElement.getElementsByTagName("input")[0].value
+                model.updateTodo(+id, { content: inputVal }).then((data) => {
+                    state.todos = state.todos.map((todo) => todo.id == data.id ? data : todo);
                 });
             }
         });
@@ -163,7 +246,14 @@ const Controller = ((view, model) => {
     const bootstrap = () => {
         init();
         handleSubmit();
-        handleDelete();
+        handlePendingListDelete();
+        handleCompletedListDelete();
+        handlePendingListStatus();
+        handleCompletedListStatus();
+        handlePendingListStartEdit();
+        handleCompletedListStartEdit();
+        handlePendingListFinishEdit();
+        handleCompletedListFinishEdit();
         state.subscribe(() => {
             view.renderTodos(state.todos);
         });
@@ -171,6 +261,6 @@ const Controller = ((view, model) => {
     return {
         bootstrap,
     };
-})(View, Model); //ViewModel
+})(View, Model);
 
 Controller.bootstrap();
